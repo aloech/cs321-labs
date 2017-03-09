d6cca9ea37a947fee81c4bb4826e0fedbaf86d2d
notenot <notsu@mail.ru>
2017-03-09 23:37:07 +0300

    Complete task 1

---
+++ MPIBegin4.cpp
@@ -0,0 +1,29 @@
+#include <windows.h>
+#pragma hdrstop
+#include "mpi.h"
+#include "pt4.h"
+void Solve()
+{
+    Task("MPIBegin4");
+    int flag;
+    MPI_Initialized(&flag);
+    if (flag == 0)
+        return;
+    int rank, size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	if (rank == 0) 
+		return;
+
+	if (rank % 2 == 0) {
+		int n;
+		pt >> n;
+		pt << (n * 2);
+	}
+	else {
+		double n;
+		pt >> n;
+		pt << (n * 2);
+	}
+}
